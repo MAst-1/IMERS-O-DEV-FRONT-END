53354a3f732515a1a6f8da82eb4dc0ec1b8ca99e
Matheus <livematgreg@gmail.com>
2024-01-26 14:33:00 -0300

    new file:   api-artists/artists.json 	new file:   index.html 	new file:   script.js 	new file:   search.js 	new file:   src/assets/icons/favicon.png 	new file:   src/assets/icons/logo-spotify.png 	new file:   src/assets/icons/search.png 	new file:   src/assets/icons/small-left.png 	new file:   src/assets/icons/small-right.png 	new file:   src/assets/playlist/1.jpeg 	new file:   src/assets/playlist/10.jpeg 	new file:   src/assets/playlist/11.jpeg 	new file:   src/assets/playlist/12.jpeg 	new file:   src/assets/playlist/13.jpeg 	new file:   src/assets/playlist/14.jpeg 	new file:   src/assets/playlist/15.jpeg 	new file:   src/assets/playlist/2.png 	new file:   src/assets/playlist/3.jpeg 	new file:   src/assets/playlist/4.jpeg 	new file:   src/assets/playlist/5.jpeg 	new file:   src/assets/playlist/6.jpeg 	new file:   src/assets/playlist/7.jpeg 	new file:   src/assets/playlist/8.jpeg 	new file:   src/assets/playlist/9.jpeg 	new file:   src/styles/main-content-rem.css 	new file:   src/styles/main-content.css 	new file:   src/styles/media-query.css 	new file:   src/styles/reset.css 	new file:   src/styles/sidebar-footer-rem.css 	new file:   src/styles/sidebar-footer.css 	new file:   src/styles/vars.css

---
+++ search.js
@@ -0,0 +1,41 @@
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((results) => displayResults(results))
+  .catch((error) => console.error("Fetch error:", error));
+}
+
+function displayResults(results) {
+  hidePlaylists();
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
+
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+  resultArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    playlistContainer.classList.remove("hidden");
+    resultArtist.classList.add("hidden");
+    return;
+  }
+  requestApi(searchTerm);
+});
